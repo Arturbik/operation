09ad6e9a293d922e38bb65a8949080596c74520b
Arturbik <onlinitan@gmail.com>
2018-12-02 22:46:44 +0700

    Выполнено 75% работы
    +Доработана операция вычетание
    +Исправлены ошибки

--- Operation/src/ru/Arturbik/math/Operation.java
+++ Operation/src/ru/Arturbik/math/Operation.java
@@ -5,8 +5,6 @@ import java.util.List;
 
 public class Operation extends Array {
 	static Array ar = new Array();
-	static int k = 0;
-	@SuppressWarnings("null")
 	public void Intersection(List array_1, List array_2) { //Выполняется операция пересечения
 		for(int i = 0; i < array_1.size(); i++){
 	        for(int j = 0; j < array_2.size(); j++){
@@ -25,12 +23,16 @@ public class Operation extends Array {
 	}
 
 	public void Subtraction(List array_1, List array_2) { //Выполняется операция вычетания
-		
-		
-	}
-
-	public void Intersection1(List array_1, List array_2) {
-		// TODO Auto-generated method stub
-		
+		for(int i = 0; i < array_1.size(); i++){
+	        for(int j = 0; j < array_2.size(); j++){
+	            if(array_1.get(i).equals(array_2.get(j)) == true){
+	               array_1.remove(i);
+	               
+	            }
+	        }
+	    }
+		for(int i = 0; i<array_1.size();i++) {
+			System.out.println(array_1.get(i)+" ");
+		}
 	}
 }
